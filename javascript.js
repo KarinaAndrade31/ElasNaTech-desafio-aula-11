aa4990df9f37496fd8ed8dc0f26fc6ba395386e3
Karina Andrade <karinnakaah31@gmail.com>
2023-10-02 16:25:25 -0300

    arquivos

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/*
+Critérios:
+
+É um número par.
+É um número positivo (maior que zero).
+É um número menor que 100.
+
+O programa deve imprimir a mensagem "O número atende aos critérios.", se o número atender a todos os critérios acima, caso contrário, deve imprimir a mensagem "O número não atende aos critérios.".
+*/
+
+var numero = parseInt(prompt("Digite um número inteiro:"));
+
+if (numero % 2 === 0 && numero > 0 && numero < 100) {
+  alert("O número atende aos critérios.");
+} else {
+  alert("O número não atende aos critérios.");
+}
